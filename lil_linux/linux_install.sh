dbc138241f70a0309c636dd52a25a791f7c8c0ca
kaz <kaz>
2025-11-19 11:51:43 -0500

    more

--- lil_linux/linux_install.sh
+++ lil_linux/linux_install.sh
@@ -11,13 +11,14 @@ echo
 
 
 
-DEP=https://raw.githubusercontent.com/LowellInstruments/dep/refs/heads/main/lil_linux/
+DEP_RAW_NO_BIN=https://raw.githubusercontent.com/LowellInstruments/dep/refs/heads/main/lil_linux/
+DEP_RAW_BIN=https://github.com/LowellInstruments/dep/raw/refs/heads/main/lil_linux/lil_linux.bin
 FOL=$HOME/.local/lil_linux
 
 
 
 echo "--------------------"
-echo "LIL linux installer v1"
+echo "LIL linux installer v2"
 echo "--------------------"
 
 
@@ -26,13 +27,13 @@ echo; echo -e "\033[1;34mlil_linux.bin file\033[0m"
 rm -rf "$FOL"
 mkdir "$FOL" || true
 cd "$FOL" || (echo "error, failed to create dir, leaving"; exit 1)
-wget -q --show-progress $DEP/lil_linux.bin
+wget -q --show-progress $DEP_RAW_BIN
 
 
 echo; echo -e "\033[1;34msvg icon\033[0m"
-wget -q --show-progress $DEP/icon_li_transparent.svg
+wget -q --show-progress $DEP_RAW_NO_BIN/icon_li_transparent.svg
 
 
 echo; echo -e "\033[1;34mDesktop file\033[0m"
-wget -q --show-progress $DEP/lil_linux.Desktop
+wget -q --show-progress $DEP_RAW_NO_BIN/lil_linux.Desktop
 
