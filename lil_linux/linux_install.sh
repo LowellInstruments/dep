abb5a3e13442296156c719786e1a02a528dac4d7
kaz <kaz>
2025-11-19 11:41:26 -0500

    more

--- lil_linux/linux_install.sh
+++ lil_linux/linux_install.sh
@@ -23,18 +23,16 @@ echo "--------------------"
 
 # either to /opt/lil_linux or $HOME
 echo; echo "lil_linux.bin file"
+rm -rf $FOL
 mkdir "$FOL" || true
 cd "$FOL" || (echo "error, failed to create dir, leaving"; exit 1)
-rm lil_linux.bin
 wget $DEP/lil_linux.bin
 
 
 echo; echo "svg icon"
-rm icon_li_transparent.svg
 wget $DEP/icon_li_transparent.svg
 
 
 echo; echo "Desktop file"
-rm lil_linux.Desktop
 wget $DEP/lil_linux.Desktop
 
