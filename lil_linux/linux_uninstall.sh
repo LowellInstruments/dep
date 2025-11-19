0a4c0614895fdf0e29d2802e554e6c58a8eb5933
kaz <kaz>
2025-11-19 12:37:56 -0500

    more

--- lil_linux/linux_uninstall.sh
+++ lil_linux/linux_uninstall.sh
@@ -8,10 +8,22 @@ echo
 FOL=$HOME/.local/lil_linux
 
 
-echo "---------------------"
-echo "LIL linux uninstaller"
-echo "---------------------"
+echo -e "\033[1;34m-------------------------\033[0m"
+echo -e "\033[1;34mLIL linux UNinstaller v05\033[0m"
+echo -e "\033[1;34m-------------------------\033[0m"
+
+
 echo
+echo "Uninstalling requires root privileges."
+sudo -v
+rv=$?
+if [ $rv -ne 0 ]; then
+    echo -e "\033[1;33mIncorrect password or root access denied.\033[0m"
+    exit 1
+fi
 
 
-rm -rf "$FOL"
+sudo rm /opt/lil_linux/lil_linux.bin
+sudo rm /usr/share/applications/lil_linux.desktop
+sudo rm /usr/share/icons/hicolor/scalable/apps/icon_li_transparent.svg
+
