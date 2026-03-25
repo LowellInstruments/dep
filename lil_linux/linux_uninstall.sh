744a91a63d4ba067ac561ad240d5b1d366a49144
kaz <kaz>
2026-03-25 13:20:48 -0400

    more

--- lil_linux/linux_uninstall.sh
+++ lil_linux/linux_uninstall.sh
@@ -4,12 +4,6 @@ clear
 echo
 
 
-# ------------------------------------------------------------------------------------------------------------------
-# you get ths installer via
-# curl https://raw.githubusercontent.com/LowellInstruments/dep/refs/heads/main/lil_linux/linux_uninstall.sh | bash
-# ------------------------------------------------------------------------------------------------------------------
-
-
 
 FOL=$HOME/.local/lil_linux
 
@@ -32,7 +26,4 @@ fi
 sudo rm /opt/lil_linux/lil_linux.bin
 sudo rm /usr/share/applications/lil_linux.desktop
 sudo rm /usr/share/icons/hicolor/scalable/apps/icon_li_transparent.svg
-
-
-echo "LIL uninstall done"
 
